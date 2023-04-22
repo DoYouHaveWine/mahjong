e9956d08eb5482e7cacfd0fc8bb9964f345c14ca
DoYouHaveWine <zhshofqian@163.com>
2023-04-22 22:47:18 +0800

    帮助

--- pages/appointment/index.vue
+++ pages/appointment/index.vue
@@ -1,376 +1,354 @@
 <template>
-	<view>
-		<view class="bg-header">
-			<u-navbar bgColor="transparent" :placeholder="true"><text slot="left"></text></u-navbar>
-			<view class="header plr-20">
-				<view class="weather-box">
-					<view>
-						<view v-if="weatherInfo.city">
+	<view class="page">
+		<!-- 门店信息 -->
+		<view class="header-box ptb-10">
+			<view class="flex-box align-center">
+				<image src="../../static/index/bg_room.png" style="width: 204rpx;height: 232rpx;"></image>
+				<view class="flex-item plr-10">
+					<view class="just-between align-center">
+						<view class="fb flex-box align-center">
+							<text>113</text>
+							<text class="plr-5">大包</text>
 							<view>
-								<text v-text="weatherInfo.city" />
-								<text class="plr-10">|</text>
-								<text v-text="weatherInfo.weather" />
+								<u-icon name="arrow-right" color="#999999" size="18"></u-icon>
 							</view>
-							<view class="centigrade" v-text="weatherInfo.temperature + '℃'" />
+						</view>
+						<view>
+							<text class="tip-doing">使用中</text>
 						</view>
 					</view>
-					<view class=""><image src="../../static/health/ic_left_top.svg" style="width: 224rpx; height:154rpx;" /></view>
+					<view class="tips-box mt10">
+						<text>空调</text>
+						<text>wifi</text>
+						<text>沙发</text>
+						<text>纸巾</text>
+						<text>独立窗户</text>
+						<text>独立洗手间</text>
+					</view>
+					<view class="just-between align-center mt10">
+						<view>
+							<view class="f-12 c-33">原价：25:00元/小时</view>
+							<view>
+								<text class="c-33 f-12">4小时套餐：</text>
+								<text class="fb f-16" style="color: #E57F70;">68.00元</text>
+							</view>
+						</view>
+						<view style="width: 140rpx;height: 68rpx;">
+							<u-button shape="circle" size="mini" customStyle="width: 100%;height: 100%;color:#4B320D">预约</u-button>
+						</view>
+					</view>
 				</view>
 			</view>
 		</view>
-		<view class="banner plr-20">
-			<image src="/static/index/ic_banner.png" style="width: 100%;height: 100%;border-radius: 5px;"></image>
+		<!-- content -->
+		<view class="content ptb-10 plr-10">
+			<!-- card -->
+			<view class="card-box bg-white">
+				<view>
+					<image src="../../static/index/shop_bg.png" style="width: 100%;height: 340rpx;"></image>
+				</view>
+				<view class="just-between align-center plr-10 ptb-10">
+					<text class="c-33 f-16">深圳-龙岗文创园店</text>
+					<view style="width: 160rpx;height: 48rpx;">
+						<u-button shape="circle" color="linear-gradient(299deg, #E5C284 0%, #F8E6CD 100%)" size="mini"
+							customStyle="width: 100%;height: 100%;color:#4B320D">切换门店></u-button>
+					</view>
+				</view>
+				<view class="just-between align-center plr-10 ptb-10">
+					<view>
+						<u--text prefixIcon="map" iconStyle="font-size: 19px;color:#cccccc" :lines="1" text="广东深圳龙岗区季华路276号文创园"
+							size="12" color="#666666"></u--text>
+						<view class="mt10">
+							<text class="distance plr-10 ptb-5 f-12">据我372.5KM</text>
+						</view>
+					</view>
+					<view class="flex-box align-center">
+						<view class="plr-10">
+							<image src="../../static/index/ic_index_dh.svg" style="width: 52rpx;height: 52rpx;"></image>
+							<view class="mt5 f-12 c-66">导航</view>
+						</view>
+						<view>
+							<image src="../../static/index/ic_index_kf.svg" style="width: 52rpx;height: 52rpx;"></image>
+							<view class="mt5 f-12 c-66">客服</view>
+						</view>
+					</view>
+				</view>
+			</view>
+			<!-- banner -->
+			<view>
+				<image src="../../static/index/banner_index.png" style="width: 100%;height: 144rpx;" mode=""></image>
+			</view>
+			<!-- todo -->
+			<view class="todo-box bg-white plr-10 ptb-10 border-box">
+				<view class="flex-box align-center">
+					<image src="../../static/index/bg_room.png" style="width: 204rpx;height: 232rpx;"></image>
+					<view class="flex-item plr-10">
+						<view class="just-between align-center">
+							<view class="fb flex-box align-center">
+								<text>113</text>
+								<text class="plr-5">大包</text>
+								<view>
+									<u-icon name="arrow-right" color="#999999" size="18"></u-icon>
+								</view>
+							</view>
+							<view>
+								<text class="tip-doing">使用中</text>
+							</view>
+						</view>
+						<view class="tips-box mt10">
+							<text>空调</text>
+							<text>wifi</text>
+							<text>沙发</text>
+							<text>纸巾</text>
+							<text>独立窗户</text>
+							<text>独立洗手间</text>
+						</view>
+						<view class="just-between align-center mt10">
+							<view>
+								<view class="f-12 c-33">原价：25:00元/小时</view>
+								<view>
+									<text class="c-33 f-12">4小时套餐：</text>
+									<text class="fb f-16" style="color: #E57F70;">68.00元</text>
+								</view>
+							</view>
+							<view style="width: 140rpx;height: 68rpx;">
+								<u-button shape="circle" size="mini" customStyle="width: 100%;height: 100%;color:#4B320D">预约</u-button>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
 		</view>
-
-		<view class="main-content">
-			<!-- 健康管理 -->
-			<view class="category-title">
-				<view class="fb title f-16">健康管理</view>
-				<view class="c-99 f-12" @click="navTo('/pagesA/healthManage/index')">更多</view>
-			</view>
-			<view class="flex-box">
-				<view class="item" v-for="(item, index) in list1" :key="index" @click="navTo(item.url)">
-					<image :src="item.src" />
-					<view class="mt5 c-33 f-14">{{ item.name }}</view>
+		<!-- 弹层 -->
+		<u-popup :show="popShow" mode="center" @close="close" @open="open" bgColor="transparent">
+			<view class="open-door border-box">
+				<image src="../../static/index/ic_index_km.png" mode=""></image>
+				<view class="ptb-20">
+					<u-button shape="circle" size="large" color="linear-gradient(179deg, #90E3B1 0%, #58AA6C 100%)" customStyle="width: 100%;height: 80rpx;color:#ffffff;">开未命名</u-button>
+					<u-button shape="circle" size="large" color="rgba(119,202,146,0.13)" customStyle="width: 100%;height: 80rpx;margin-top:40rpx;border: 2rpx solid #6DA772;color:#58AA6C;">开包间-房门</u-button>
 				</view>
 			</view>
-
-			<!-- 健康数据 -->
-			<view class="category-title">
-				<view class="fb title f-16">健康数据</view>
-				<view class="c-99 f-12" @click="navTo('/pagesA/healthManage/healthData', true, true)">更多</view>
-			</view>
-			<view class="flex-box">
-				<view class="item" v-for="(item, index) in list2" :key="index" @click="navTo(item.url)">
-					<image :src="item.src" />
-					<view class="mt5 c-33 f-14">{{ item.name }}</view>
-				</view>
-			</view>
-
-			<!-- 健康工具 -->
-			<view class="category-title">
-				<view class="fb title f-16">健康工具</view>
-				<!-- <view class="c-99 f-12">更多</view> -->
-			</view>
-			<view class="flex-box">
-				<view
-					v-if="!item.hidden"
-					class="item"
-					v-for="(item, index) in list3"
-					:key="index"
-					:class="{ 'dev-func-style': item.dev }"
-					@click="navTo(item.url, true, true, item.dev)"
-				>
-					<image :src="item.src" />
-					<view class="mt5 c-33 f-14">{{ item.name }}</view>
-				</view>
-			</view>
-		</view>
-
+		</u-popup>
 		<!-- 底部tabbar -->
 		<tabbar :tabList="tabbarList" />
 	</view>
 </template>
 
 <script>
-import { refreshCardList } from '@/common/http/api.js';
-import { getVertionType } from '@/common/utils/commonUtil.js';
-export default {
-	data() {
-		return {
-			weatherInfo: {},
-			list1: [
-				{
-					name: '高血压',
-					src: '/static/health/ic_gxy.svg',
-					url: '/pagesA/healthManage/index?tabIndex=0'
-				},
-				{
-					name: '糖尿病',
-					src: '/static/health/ic_tnb.svg',
-					url: '/pagesA/healthManage/index?tabIndex=1'
-				},
-				{
-					name: '冠心病',
-					src: '/static/health/ic_gxb.svg',
-					url: '/pagesA/healthManage/index?tabIndex=2'
-				},
-				{
-					name: '慢阻肺',
-					src: '/static/health/ic_mzf.svg',
-					url: '/pagesA/healthManage/index?tabIndex=3'
-				}
-			],
-			list2: [
-				{
-					name: '血压',
-					src: '/static/health/ic_xy.svg',
-					url: '/pagesA/healthManage/gxy/manage'
-				},
-				{
-					name: '血糖',
-					src: '/static/health/ic_xt.svg',
-					url: '/pagesA/healthManage/bloodSuger/manage'
-				},
-				{
-					name: '体重',
-					src: '/static/health/ic_tz.svg',
-					url: '/pagesA/healthManage/weight/manage'
-				},
-				{
-					name: '心率',
-					src: '/static/health/ic_xl.svg',
-					url: '/pagesA/healthManage/rate/manage',
-					needToken: true,
-					needCard: true
-				}
-			],
-			list3: [
-				// {
-				// 	name: '健康问答',
-				// 	src: '/static/health/ic_jkwd.svg',
-				// 	url: '',
-				// 	dev: true,
-				// 	hidden: getVertionType() == 'release'
-				// },
-				// {
-				// 	name: '医学百科',
-				// 	src: '/static/health/ic_yxbk.svg',
-				// 	url: '',
-				// 	dev: true,
-				// 	hidden: getVertionType() == 'release'
-				// },
-				{
-					name: '用药提醒',
-					src: '/static/health/ic_yytx.svg',
-					url: '/pagesA/healthManage/drug/remind',
-					needToken: true,
-					needCard: true
-				},
-				// {
-				// 	name: '监测提醒',
-				// 	src: '/static/health/ic_jctx.svg',
-				// 	url: '',
-				// 	dev: true,
-				// 	hidden: getVertionType() == 'release'
-				// }
-			]
-		};
-	},
+	import {
+		refreshCardList,
+		getUnreadMsgNum,
+		getInfoArticlePage,
+		getAppUpdate
+	} from '@/common/http/api.js';
+	export default {
+		data() {
+			return {
+				popShow: false,
 
-	computed: {
-		tabbarList() {
-			return this.$store.state.vuex_tabbar;
-		},
-
-		cardList() {
-			return this.$store.state.cardList;
-		}
-	},
-
-	onShow() {
-		if (uni.getStorageSync('token')) {
-			refreshCardList();
-		}
-		this.getWeather();
-	},
-
-	methods: {
-		/**
-		 * 页面跳转
-		 * @param {String} url 跳转URL
-		 * @param {Bool} needToken 是否需要检查登录（获取token）
-		 * @param {Bool} needCard 是否需要健康卡
-		 */
-		async navTo(url, needToken = true, needCard = true, dev) {
-			if (dev) {
-				// 建设中。。。
-				this.$devFuncClick();
-				return;
-			}
-			if (needCard) {
-				// 跳转需要先建卡时必定需要token
-				needToken = true;
-			}
-			if (needToken && !uni.getStorageSync('token')) {
-				// 需要Token但未获取Token
-				this.navToAuth();
-				return;
-			}
-
-			if (needCard && !this.checkBindCard()) {
-				// 需要绑卡但未绑卡
-				return;
-			}
-
-			uni.navigateTo({ url });
-		},
-
-		/** 检查是否绑定电子健康卡,并做未绑卡提示 */
-		checkBindCard() {
-			if (this.cardList.length === 0) {
-				uni.showModal({
-					title: '提示',
-					content: '请先绑定电子健康卡再进行相应操作！',
-					success: res => {
-						if (res.confirm) {
-							uni.navigateTo({ url: '/pagesA/card/list' });
-						}
+				searchKey: '',
+				unreadMsgNum: '',
+				funcList: [{
+						text: '开门',
+						src: '/static/index/ic_index_km.svg',
+						url: '',
+						hidden: false // 是否隐藏
+					},
+					{
+						text: '续单',
+						src: '/static/index/ic_index_xd.svg',
+						url: ''
+					},
+					{
+						text: '美团验券',
+						src: '/static/index/ic_index_mtyq.svg',
+						type: 1,
+						url: ''
+					},
+					{
+						text: '抖音验券',
+						src: '/static/index/ic_index_dyyq.svg',
+						type: 1,
+						url: ''
 					}
-				});
-				return false;
+				]
+			};
+		},
+
+		computed: {
+			tabbarList() {
+				return this.$store.state.vuex_tabbar;
 			}
-			return true;
 		},
 
-		/** 跳转到去授权的页面
-		 * @param {Object} fromPage 跳到auth授权的页面来源，例如mine
-		 */
-		navToAuth(fromPage = 'health') {
-			// #ifdef MP-WEIXIN
-			uni.reLaunch({ url: `/pagesA/auth/auth?fromPage=${fromPage}` });
-			// #endif
-			
-			// #ifndef MP-WEIXIN
-			uni.reLaunch({ url: `/pagesA/login/login?fromPage=${fromPage}` });
-			// #endif
+		onLoad(options) {
+
+		},
+		onShow() {
+
 		},
 
-		getWeather() {
-			uni.request({
-				url: 'https://restapi.amap.com/v3/weather/weatherInfo',
-				method: 'GET',
-				data: {
-					key: '508a2ae0ca620fdcbe9c99a3fa58b312',
-					city: '130600',
-					extensions: 'base',
-					output: 'JSON'
-				},
-				success: res => {
-					this.weatherInfo = res.data.lives[0];
-				},
-				fail(err) {
-					console.log(err);
+		methods: {
+			open() {
+				this.popShow = true
+			},
+			close() {
+				this.popShow = false
+			},
+			/**
+			 * 页面跳转
+			 * @param {String} url 跳转URL
+			 * @param {Bool} needToken 是否需要检查登录（获取token）
+			 * @param {Bool} needCard 是否需要健康卡
+			 */
+			async navTo(url, needToken = false, needCard = false) {
+				if (needCard) {
+					// 跳转需要先建卡时必定需要token
+					needToken = true;
 				}
-			});
+				if (needToken && !uni.getStorageSync('token')) {
+					// 需要Token但未获取Token
+					this.navToAuth();
+					return;
+				}
+
+				if (needCard && !this.checkBindCard()) {
+					// 需要绑卡但未绑卡
+					return;
+				}
+
+				uni.navigateTo({
+					url
+				});
+			},
+			/**
+			 * 功能点击回调
+			 *
+			 * @param {Object} item 功能项
+			 */
+			async onFuncClick(item) {
+				if (item.dev) {
+					// 建设中...
+					this.$devFuncClick();
+					return;
+				}
+				if (item.url) {
+					if (type == 1) {
+						uni.switchTab({
+							url: item.url
+						});
+					} else {
+						uni.navigateTo({
+							url: item.url
+						});
+					}
+				} else {
+					switch (item.text) {
+						case '开门':
+							this.popShow = true
+							break;
+					}
+				}
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="scss">
-page {
-	.bg-header {
-		position: relative;
-		overflow: hidden;
-		&::after {
-			width: 140%;
-			height: 100%;
-			position: absolute;
-			left: -20%;
-			top: 0;
-			z-index: -1;
-			content: '';
-			border-radius: 0 0 50% 50%;
-			background: linear-gradient(180deg, #66acff 0%, #3683ff 99.99%, #4298ff 100%);
-		}
-	}
+	page {
+		.header-box {
+			background: linear-gradient(180deg, #58AA6C 0%, #84C794 100%);
 
-	.header {
-		height: 340rpx;
-		color: #fff;
-		overflow: hidden;
-
-		.weather-box {
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-
-			.centigrade {
-				font-size: 40rpx;
-			}
-		}
-
-		.tip-box {
-			height: 100rpx;
-			border-radius: 20rpx;
-			background: rgba(0, 0, 0, 0.1);
-			display: flex;
-			box-sizing: border-box;
-
-			.tip-items {
-				flex: 1;
+			.func-grid {
+				overflow: hidden;
+				width: 100%;
 				display: flex;
-				align-items: center;
+				flex-wrap: wrap;
 
-				image {
-					width: 60rpx;
-					height: 60rpx;
-				}
+				.item {
+					display: flex;
+					width: 25%;
+					height: 160rpx;
+					text-align: center;
+					align-items: center;
+					flex-direction: column;
+					justify-content: center;
 
-				.item-index {
-					color: #e2e2e2;
-					font-family: PingFang SC;
-					font-size: 20rpx;
-				}
-			}
-		}
-	}
+					image {
+						width: 52rpx;
+						height: 56rpx;
+					}
 
-	.banner {
-		position: relative;
-		top: -160rpx;
-		height: 240rpx;
-		border-radius: 5px;
-		overflow: hidden;
-	}
-
-	.main-content {
-		position: relative;
-		top: -160rpx;
-
-		.category-title {
-			padding: 20rpx 40rpx;
-			display: flex;
-			justify-content: space-between;
-
-			.title {
-				position: relative;
-
-				&::after {
-					width: 50%;
-					height: 16rpx;
-					opacity: 0.2;
-					content: '';
-					border-radius: 30rpx;
-					background: #4298ff;
-					position: absolute;
-					bottom: -5rpx;
-					left: 0;
+					text {
+						color: #fff;
+						font-family: PingFang SC-Semibold;
+						font-weight: 500;
+						font-size: 28rpx;
+						margin-top: 12rpx;
+					}
 				}
 			}
 		}
 
-		.flex-box {
-			display: flex;
-			width: 100%;
-			flex-flow: wrap;
+		.content {
+			.card-box {
+				box-shadow: 2rpx 6rpx 30rpx 2rpx rgba(18, 80, 33, 0.1);
+				border-radius: 16rpx 16rpx 16rpx 16rpx;
 
-			.item {
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-				width: 25%;
-				padding: 20rpx;
-				box-sizing: border-box;
-				text-align: center;
+				.distance {
+					border-radius: 10rpx 10rpx 10rpx 10rpx;
+					border: 1px solid #6DA772;
+					color: #58AA6C;
+				}
+			}
 
-				image {
-					width: 70rpx;
-					height: 70rpx;
+			.todo-box {
+				box-shadow: 2rpx 6rpx 30rpx 2rpx rgba(109, 75, 23, 0.1);
+				border-radius: 16rpx 16rpx 16rpx 16rpx;
+
+				.tip-doing {
+					color: #D9A661;
+					background: rgba(217, 166, 97, 0.12);
+					border-radius: 8rpx 8rpx 8rpx 8rpx;
+					opacity: 1;
+					border: 1rpx solid #D9A661;
+					padding: 4rpx 6rpx;
+					font-size: 24rpx;
+				}
+
+				.tips-box {
+					display: flex;
+					flex-wrap: wrap;
+					flex-direction: row;
+					justify-content: space-between;
+
+					text {
+						border-radius: 10rpx 10rpx 10rpx 10rpx;
+						border: 2rpx solid #B9E0BD;
+						color: #B9E0BD;
+						padding: 4rpx 6rpx;
+						margin-top: 10rpx;
+						font-size: 24rpx;
+					}
 				}
 			}
 		}
+		.open-door{
+			width: 520rpx;
+			height: 404rpx;
+			padding: 100rpx 40rpx;
+			background: linear-gradient(180deg, #D1E8E3 0%, #FFFFFF 100%);
+			border-radius: 20rpx 20rpx 20rpx 20rpx;
+			position: relative;
+			
+			image{
+				width: 256rpx;
+				height: 256rpx;
+				position: absolute;
+				top: -128rpx;
+				left: 50%;
+				transform: translateX(-50%);
+			}
+		}
 	}
-}
 </style>
---
+++ pagesA/help/index.vue
@@ -0,0 +1,102 @@
+<template>
+	<view class="mine-page">
+		<view class="">
+			<image src="../../static/help/banner_help.png" style="width: 100%;height: 280rpx;"></image>
+		</view>
+		<view class="mt10 f-14 c-33">
+			介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍
+		</view>
+		<view class="c-33 f-18 fb mt10">预约使用教程</view>
+		<view class="mt10">
+			<view class="steps item-1 mt5">
+				<view class="plr-15">
+					<view class="c-58AA6C f-16">01.小程序预约</view>
+					<view class="c-66 f-12 mt10">小程序预约下单，并完成支付</view>
+				</view>
+			</view>
+			<view class="help-next tc mt5">
+				<image src="../../static/help/help_next.svg"></image>
+			</view>
+			<view class="steps item-2 mt5">
+				<view class="plr-15">
+					<view class="c-58AA6C f-16">02.点击‘确认到店’开门</view>
+					<view class="c-66 f-12 mt10">到达门店后，点击“确认到店”开始订单并开门</view>
+				</view>
+			</view>
+			<view class="help-next tc mt5">
+				<image src="../../static/help/help_next.svg"></image>
+			</view>
+			<view class="steps item-3 mt5">
+				<view class="plr-15">
+					<view class="c-58AA6C f-16">03.找到预约房间</view>
+					<view class="c-66 f-12 mt10">进店后，找到您所预约的房间，享受美好时光</view>
+				</view>
+			</view>
+			<view class="help-next tc mt5">
+				<image src="../../static/help/help_next.svg"></image>
+			</view>
+			<view class="steps item-4 mt5">
+				<view class="plr-15">
+					<view class="c-58AA6C f-16">04.离店自动结算</view>
+					<view class="c-66 f-12 mt10">时间到，携带好随身物品离店自动结算</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		computed: {},
+
+		onShow() {},
+
+		onLoad() {},
+
+		methods: {}
+	};
+</script>
+
+<style lang="scss">
+	.mine-page {
+		padding: 20rpx;
+		box-sizing: border-box;
+		.steps{
+			width: 100%;
+			height: 144rpx;
+			display: flex;
+			align-items: center;
+			.c-58AA6C{
+				color: #58AA6C;
+			}
+		}
+		& .item-1{
+			background: url('../../static/help/help_step1.png') no-repeat;
+			background-size: 100% 100%;
+		}
+		& .item-2{
+			background: url('../../static/help/help_step2.png') no-repeat;
+			background-size: 100% 100%;
+		}
+		& .item-3{
+			background: url('../../static/help/help_step3.png') no-repeat;
+			background-size: 100% 100%;
+		}
+		& .item-4{
+			background: url('../../static/help/help_step4.png') no-repeat;
+			background-size: 100% 100%;
+		}
+		.help-next{
+			image{
+				width: 28rpx;
+				height: 40rpx;
+			}
+		}
+	}
+	
+</style>
